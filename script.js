803659e0748cf0980c5b755ede28b63d811d7b29
shoamri joseph <shomarijoseph@gmail.com>
2022-11-26 16:44:34 -0500

    created remove book button

--- script.js
+++ script.js
@@ -17,20 +17,45 @@ function addBookToLibrary(title, author, pages, read) {
 //Display books on page
 function displayBooks() {
   const library = document.getElementsByClassName("library");
-
+let index = 0;
   const removeDivs = document.querySelectorAll(".card");
   for (let i = 0; i < removeDivs.length; i++) {
     removeDivs[i].remove();
   }
+  const removeBookButton = document.createElement('button');
+  removeBookButton.classList.add('remove-book-button');
+  removeBookButton.textContent = 'Remove From Library'
+  console.log('show me the current array:', myLibrary);
 
-  myLibrary.forEach((myLibrary) => {
+
+
+
+  myLibrary.forEach((myLibrarys) => {
+    
     const card = document.createElement("div");
     card.classList.add("card");
     library[0].appendChild(card);
-    for (let key in myLibrary) {
-      console.log(`${key}: ${myLibrary[key]}`);
+
+    //Create Remove book Button 
+    const removeBookButton = document.createElement("button");
+    removeBookButton.classList.add("remove-book-button");
+    removeBookButton.textContent = "Remove From Library";
+    console.log("show me the current array:", myLibrary);
+
+
+
+
+
+    index++;
+    const num = document.createElement("h1");
+    num.textContent = index;
+    card.appendChild(num);
+    card.appendChild(removeBookButton)
+
+    for (let key in myLibrarys) {
+      console.log(`${key}: ${myLibrarys[key]}`);
       const para = document.createElement("p");
-      para.textContent = `${key}: ${myLibrary[key]}`;
+      para.textContent = `${key}: ${myLibrarys[key]}`;
       card.appendChild(para);
     }
   });
@@ -39,9 +64,11 @@ function displayBooks() {
 //create pop up form
 function openForm() {
   document.getElementById("popForm").style.display = "block";
+  document.getElementById("add").style.display = "none";
 }
 function closeForm() {
   document.getElementById("popForm").style.display = "none";
+  document.getElementById("add").style.display = "block";
 }
 
 //take in user submitted data
